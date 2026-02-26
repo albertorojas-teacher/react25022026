65bf0771721c481cc9c41b06b7d5bbb284045d37
Alberto Rojas Gallegos <albertorojas@profesorado.cei.es>
2026-02-26 19:59:28 +0100

    feat: integrate Tailwind CSS for styling and update dependencies
    - Added Tailwind CSS and its Vite plugin to package.json
    - Updated Aranceles component to use Tailwind CSS classes for styling
    - Imported Tailwind CSS in index.css
    - Configured Vite to use Tailwind CSS plugin

--- src/components/Aranceles/Aranceles.tsx
+++ src/components/Aranceles/Aranceles.tsx
@@ -3,8 +3,8 @@ function Aranceles() {
     const paragraph = 'CNN - El Gobierno está obligado a reembolsar US$ 134.000 millones (y la cifra sigue aumentando) en ingresos recaudados por los amplios aranceles del presidente Donald Trump que la Corte Suprema declaró ilegales la semana pasada.'
   return (
     <div>
-      <h1 className="title">{title}</h1>
-      <p className="paragraph">{paragraph}</p>
+      <h1 className="text-3xl font-bold underline">{title}</h1>
+      <p className="text-blue-600/25 dark:text-sky-400/25">{paragraph}</p>
     </div>
   )
 }
